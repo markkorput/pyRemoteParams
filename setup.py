8d10d7b4a9a6a4abd7a4a17b5151206802289fb6
mark <mark.korput@gmail.com>
2020-03-10 19:02:36 +0100

    LICENSE, setup.cfg, more setup.py updates

--- setup.py
+++ setup.py
@@ -9,11 +9,19 @@ setup(name='remote_params',
       license='MIT',
       install_requires=[
             'evento>=1.0.2',
-            # 'oscpy' # added embedded copy of oscpy, with some bind_all patch
+            # 'oscpy' # added embedded copy of oscpy, with bind_all patch
             'websockets>=8.1'
       ],
       zip_safe=False,
       include_package_data=True,
       test_suite='nose.collector',
       tests_require=['nose'],
-      )
+      classifiers=[
+            'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            'License :: OSI Approved :: MIT License',   # Again, pick a license
+            'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+      ])
+
