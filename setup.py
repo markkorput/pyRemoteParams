b96cc3d694f5715af40cf71865e7c2f550897a81
mark <mark.korput@gmail.com>
2020-05-24 21:30:20 +0200

    remove some classifiers from setup.py

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ setup(name='remote_params',
             'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
             'License :: OSI Approved :: MIT License',   # Again, pick a license
             'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
       ])
 
